0f06bd4e9949b014feeac18eca594be12dad6a45
clairecky <78159773+clairecky@users.noreply.github.com>
2021-03-18 10:22:45 +0800

    Add files via upload

---
+++ PageContent.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <ul>
+        <li v-for="item in itemsList" v-bind:key="item.name">
+
+            <h2>{{item.name}}</h2>
+            <img v-bind:src="item.image"/>
+            <p>{{item.price}}</p>   
+            <qtyCounter></qtyCounter>        
+        </li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+import QuantityCounter from './components/QuantityCounter.vue'
+
+export default {
+  props:{
+      itemsList:{
+          type:Array
+      }
+  },
+  components: {
+    'qtyCounter': QuantityCounter,
+  }
+}
+</script>
+
+<style scoped>
+#itemsList {
+  width: 100%;
+  max-width: 70%;
+  margin: 0px;
+  padding: 0 5px;
+  box-sizing: border-box;
+}
+ul {
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  flex-grow: 1;
+  flex-basis: 300px;
+  text-align: center;
+  padding: 10px;
+  border: 1px solid #222;
+  margin: 10px;
+}
+img {
+  width: 135px;
+  height: 135px;
+}
+
+#price {
+  font-size: 30px;
+}
+
+#itemName {
+  font-size: 30px;
+}
+
+#shoppingBasket {
+  position: absolute;
+  top: 23%;
+  left: 78%;
+}
+</style>
---
+++ QuantityCounter.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <button v-on:click="counter--">-</button>
+        <span> {{totalCount()}} </span>
+        <button v-on:click="counter++">+</button>
+    </div>
+
+</template>
+    
+
+<script>
+export default {
+    data() {
+        return {
+            counter: 0,
+        }
+    },
+    methods: {
+        totalCount: function() {
+            if (this.counter <= 0) {
+                this.counter = 0
+            } else if (this.counter >= 10) {
+                this.counter = 10
+            }
+            return this.counter
+        },
+    }
+}
+</script>
+
+<style>
+    button{
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        text-align: center; 
+        background-color: #E9AFE3;
+        color: #020202; 
+        font-size: 18px;
+        padding: 3px 5px;
+        border: none;
+    }
+</style>
